0bf40a16a4b8b8e3d702d8c6632692ff86e2e152
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-09-21 12:31:49 -1000

    Moving on with pack Get, added demo func in main

--- nftlabs/pack.go
+++ nftlabs/pack.go
@@ -1,8 +1,13 @@
 package nftlabs
 
 import (
+	"errors"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/nftlabs/nftlabs-sdk-go/abi"
+	"log"
 	"math/big"
 )
 
@@ -13,12 +18,58 @@ type PackSdk interface {
 	GetNfts(packId big.Int) ([]PackNft, error)
 	Balance(tokenId big.Int) (big.Int, error)
 	BalanceOf(address string, tokenId string) uint64
-	Transfer(to string, tokenId string, quantity big.Int) (error)
+	Transfer(to string, tokenId string, quantity big.Int) error
 }
 
 type PackSdkModule struct {
 	Client *ethclient.Client
 	Address string
 	Options *SdkOptions
-	caller *abi.NFTCaller
+	gateway Gateway
+	caller *abi.PackCaller
+}
+
+func NewPackSdkModule(client *ethclient.Client, address string, opt *SdkOptions) (*PackSdkModule, error) {
+	if opt.IpfsGatewayUrl == "" {
+		opt.IpfsGatewayUrl = "https://cloudflare-ipfs.com/ipfs/"
+	}
+
+	caller, err := abi.NewPackCaller(common.HexToAddress(address), client)
+	if err != nil {
+		return nil, err
+	}
+
+	// internally we force this gw, but could allow an override for testing
+	var gw Gateway
+	gw = NewCloudflareGateway(opt.IpfsGatewayUrl)
+
+	return &PackSdkModule{
+		Client: client,
+		Address: address,
+		Options: opt,
+		gateway: gw,
+		caller: caller,
+	}, nil
+}
+
+func (sdk *PackSdkModule) Get(packId *big.Int) (Pack, error) {
+	packUri, err := sdk.caller.TokenURI(&bind.CallOpts{}, packId)
+	if err != nil {
+		return Pack{}, err
+	}
+
+	if packUri == "" {
+		return Pack{}, errors.New(fmt.Sprintf("Could not find pack with id %d", packId))
+	}
+
+	body, err := sdk.gateway.Get(packUri)
+	if err != nil {
+		return Pack{}, err
+	}
+
+	//metadata :=
+
+	log.Printf("body = %v", string(body))
+	
+	return Pack{}, nil
 }
