5f5edb24850b82376e5c98994adf3c14e5206a81
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-09-29 09:41:59 -1000

    Added currency.mint and currency.totalsupply to cli

--- cmd/nftlabs/currency_commands.go
+++ cmd/nftlabs/currency_commands.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"log"
+	"math/big"
+	"strconv"
 )
 
 var (
@@ -31,7 +34,55 @@ var currencyGetCmd = &cobra.Command{
 	},
 }
 
+var currencyMintCmd = &cobra.Command{
+	Use: "mint [amount]",
+	Short: "Mints [amount] of currency at the specified address",
+	Args: cobra.ExactArgs(1),
+	ValidArgs: []string{"amount"},
+	Run: func(cmd *cobra.Command, args []string) {
+		amount, err := strconv.ParseInt(args[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		module, err := getCurrencyModule()
+		if err != nil {
+			panic(err)
+		}
+
+		currency, err := module.Get()
+		if err != nil {
+			panic(err)
+		}
+
+		if err := module.Mint(big.NewInt(amount)); err != nil {
+			panic(err)
+		}
+		log.Printf("Minted %d of %v\n", amount, currency.Symbol)
+	},
+}
+
+var currencyTotalSupplyCmd = &cobra.Command{
+	Use: "totalSupply",
+	Short: "Gets the total supply of the currency at the specified address",
+	Run: func(cmd *cobra.Command, args []string) {
+		module, err := getCurrencyModule()
+		if err != nil {
+			panic(err)
+		}
+
+		totalSupply, err := module.TotalSupply()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(totalSupply)
+	},
+}
+
 func init() {
+	currencyCmd.AddCommand(currencyTotalSupplyCmd)
+	currencyCmd.AddCommand(currencyMintCmd)
 	currencyCmd.AddCommand(currencyGetCmd)
 
 	currencyCmd.PersistentFlags().StringVarP(&currencyContractAddress, "address", "a", "", "currency contract address")
